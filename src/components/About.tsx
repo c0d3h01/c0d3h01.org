f6d5070d0d774e2b7d191e925d52a9175e57e416
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-27 12:41:11 +0530

    chore(About): wording and update skills section in About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,23 +3,18 @@ const About: React.FC = () => {
       <section id="about" className="py-20 w-full max-w-3xl">
         <h2 className="text-3xl font-bold mb-6 text-left">About Me</h2>
         <p className="text-gray-400 leading-relaxed">
-          I am a passionate software developer with a strong focus on creating high-quality, user-friendly applications.
-          My core skills include <code>C/CPP | Python</code>. I am a continuous learner and always strive to stay
+          I am a like to work on softwares with a strong focus on creating high-quality, user-friendly applications, stuffs.
+          My core languages includes <code>C++, Python</code>. I am a continuous learner and always strive to stay
           up-to-date with the latest technologies and best practices.
         </p>
   
         <div className="mt-6 text-left">
-          <h3 className="text-3xl font-bold mb-3">Skills</h3>
+          <h3 className="text-3xl font-bold mb-3">My Knowing languages</h3>
           <ul className="flex flex-wrap gap-2">
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">C/CPP</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">Python</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">nix</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">git</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">shell</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">Node.js</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">Tailwind CSS</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">SQL</li>
-            <li className="bg-gray-800 text-gray-300 px-3 py-1 rounded-lg">MongoDB</li>
+            <li className="bg-gray-800 text-yellow-300 px-3 py-1 rounded-lg">C / C++</li>
+            <li className="bg-gray-800 text-yellow-300 px-3 py-1 rounded-lg">Rust</li>
+            <li className="bg-gray-800 text-yellow-300 px-3 py-1 rounded-lg">Python</li>
+            <li className="bg-gray-800 text-yellow-300 px-3 py-1 rounded-lg">nix</li>
           </ul>
         </div>
       </section>
